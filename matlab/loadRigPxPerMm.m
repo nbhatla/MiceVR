a8e2075694d91f1e4a33986c3bf4fee51db89d25
ucsfidl <ucsfidl@gmail.com>
2022-05-20 09:36:35 -0700

    Update loadRigPxPerMm.m

--- matlab/loadRigPxPerMm.m
+++ matlab/loadRigPxPerMm.m
@@ -5,6 +5,6 @@ rigPxPerMm = [42 38;       % rig 1
              57 49;        % rig 3
              60.4 64.8;    % rig 4 - updated 1/2/21
              51.48 52.52;  % rig 5 - updated 7/19/21
-             58.92 58.43;]; % rig 6 - updated 9/21/21
+             55.31 63.94;]; % rig 6 - updated 5/20/22
 
 end
