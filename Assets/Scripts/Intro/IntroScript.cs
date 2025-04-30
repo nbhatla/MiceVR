b9cf9d3b31c88aa567d72b6db293ccf221eb76a3
nbhatla <nikhil@superfacts.org>
2025-04-30 15:05:21 -0700

    a

--- Assets/Scripts/Intro/IntroScript.cs
+++ Assets/Scripts/Intro/IntroScript.cs
@@ -1,7 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+
 public class IntroScript : MonoBehaviour {
+
+	public UDPSend udpSender;
+
+	public void Awake()
+	{
+		// This sequence is necessary to initiate the connection, which will reset the arduino
+        this.udpSender.ForceStopSolenoid();
+        this.udpSender.CheckReward();  // DO NOT DELETE - without this, the BlowerOn command fails
+
+		// Arduino reset needs time to settle before it can respond to commands
+		System.Threading.Thread.Sleep(3000);
+		this.udpSender.SendBlowerOn();
+	}
 
 	public void LoadGenerator()
     {
@@ -30,6 +45,7 @@ public class IntroScript : MonoBehaviour {
 
 	public void Exit()
     {
+		this.udpSender.SendBlowerOff();
         Application.Quit();
     }
 }
